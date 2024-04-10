85b1e73a59111cf66fd9e2811134a759cd6cbb8a
Anna Markova <anbeley@mail.ru>
2024-04-10 20:27:58 +0300

    Change test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -7,7 +7,7 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldReturnRemainValue0() {
-        CashbackHackService service = new git statusCashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
