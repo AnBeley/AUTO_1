486f1bc3cd255d987849670196b62b0281023e03
Anna Markova <anbeley@mail.ru>
2024-04-11 10:56:35 +0300

    Testing 1st test

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -5,14 +5,14 @@ import static org.testng.AssertJUnit.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @Test
-    public void shouldReturnRemainValue0() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(actual, expected);
-    }
+    //@Test
+    //public void shouldReturnRemainValue0() {
+      //  CashbackHackService service = new CashbackHackService();
+        //int amount = 1000;
+        //int actual = service.remain(amount);
+        //int expected = 0;
+        //assertEquals(actual, expected);
+    //}
 
     @Test
     public void shouldReturnRemainValue766() {
